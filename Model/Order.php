1ada227375f07ede012ee9b19c8e17737742e842
CSteigueber <c.steigueber@gmail.com>
2020-01-16 10:19:03 +0100

    increased readability

--- Model/Order.php
+++ Model/Order.php
@@ -2,19 +2,19 @@
 Class Order {
     public $id;
     public $customer_id;
-    public $items=array();
+    public $items = array();
     public $total;
     public $cat1; # count the amount of category1 items in the order
     public $cat2; # count the amount of category2 items in the order
     public $cheapestCat1Object;
-    public $discount1=false;
-    public $discount2=false;
-    public $discountLoyal=false;
+    public $discount1 = false;
+    public $discount2 = false;
+    public $discountLoyal = false;
     public function ConvertInputToOrder($input){
-        $this->id=$input->id;
-        $this->customer_id=$input->{'customer-id'};
-        $this->items=$input->items;
-        $this->total=$input->total;
+        $this->id = $input->id;
+        $this->customer_id = $input->{'customer-id'};
+        $this->items = $input->items;
+        $this->total = $input->total;
       }
       
     public function GetProductDetailsIntoOrder($products){
@@ -24,8 +24,8 @@ Class Order {
             # Outsourcing it into another function would ease overview and maintenance.
             # But it would reduce performance, as the same list would be itterated over again.
             if ($product->id == $item->{'product-id'}){
-              $item->category=$product->category;
-              $item->description=$product->description;
+              $item->category = $product->category;
+              $item->description = $product->description;
             break; #avoiding that the algorithm runs through a long list of products after already matching the id from the this
           }
           
@@ -44,26 +44,26 @@ Class Order {
       }
     public function discountCheapestCat1(){
         foreach ($this->items as $item) {
-          if ($item->category=="1" && ($item->{'unit-price'} < $this->cheapestCat1Object || $this->cheapestCat1Object==0)){
-            $this->cheapestCat1Object=$item->{'unit-price'};
-            $this->discount1=true;
+          if ($item->category == "1" && ($item->{'unit-price'} < $this->cheapestCat1Object || $this->cheapestCat1Object == 0)){
+            $this->cheapestCat1Object = $item->{'unit-price'};
+            $this->discount1 = true;
           }
         }
-        $this->total-=$this->cheapestCat1Object*0.2;
+        $this->total -= $this->cheapestCat1Object * 0.2;
 
       }
     public function giveFreeCat2() {
         foreach ($this->items as $item) {
-          if ($item->category=="2"){
-            $item->quantity+= floor($item->quantity/5);
-            $this->discount2=true;
+          if ($item->category == "2"){
+            $item->quantity += floor($item->quantity/5);
+            $this->discount2 = true;
           }
         }
 
       }
     public function LoyalCustomerDiscount(){
-        $this->total*=0.9;
-        $this->discountLoyal=true;
+        $this->total *= 0.9;
+        $this->discountLoyal = true;
     }
 
 }
--- Model/model.php
+++ Model/model.php
@@ -5,7 +5,7 @@ require 'Order.php';
 # -----------------------functions-------------------------------------
 function findCustomerById($customers,$id){
   foreach ($customers as $customer) {
-    if ($customer->id==$id) 
+    if ($customer->id == $id) 
     {
       return $customer;
      }
@@ -19,11 +19,11 @@ function findCustomerById($customers,$id){
 
 # ---------------------------Program start ----------------------------
 # Get input:
-$products=json_decode(file_get_contents("../data/products.json"));
-$customers=json_decode(file_get_contents("../data/customers.json"));
-$input=json_decode(file_get_contents("../example-orders/order3.json"));
+$products = json_decode(file_get_contents("../data/products.json"));
+$customers = json_decode(file_get_contents("../data/customers.json"));
+$input = json_decode(file_get_contents("../example-orders/order3.json"));
 
-$order= new Order();
+$order = new Order();
 $order->ConvertInputToOrder($input);
 $customer=findCustomerById($customers,$order->customer_id);
 $order->GetProductDetailsIntoOrder($products);
@@ -36,24 +36,24 @@ if ($order->cat1 > 1){
 if ($order->cat2 >= 5){
   $order->giveFreeCat2();
 }
-if ($customer->revenue>=1000){
+if ($customer->revenue >= 1000){
   $order->LoyalCustomerDiscount();
 }
 
 # create the output:
-$message="</br>You have to pay ".round($order->total,2)." EUR. You recieved the following discount(s): </br>";
-if ($order->discount1==true){
-   $message.="discount 1: You pay less for the cheapest item</br>";
+$message = "</br>You have to pay ".round($order->total,2)." EUR. You recieved the following discount(s): </br>";
+if ($order->discount1 == true){
+   $message .= "discount 1: You pay less for the cheapest item</br>";
 }                  
-if ($order->discount2==true){
-   $message.="discount 2: You get one or more additionel items free of charge</br>"; 
+if ($order->discount2 == true){
+   $message .= "discount 2: You get one or more additionel items free of charge</br>"; 
 }
-if ($order->discountLoyal==true){
-   $message.="Loyal discount: You pay 10% less for the entire order </br>";
+if ($order->discountLoyal == true){
+   $message .= "Loyal discount: You pay 10% less for the entire order </br>";
 } 
-if ($order->discount1==false && $order->discount2==false && $orderLoyal==false){
-  $message.="none</br>";
+if ($order->discount1 == false && $order->discount2 == false && $orderLoyal == false){
+  $message .= "none</br>";
 }      
 
-$message.="Thank you for your order!";
+$message .= "Thank you for your order!";
 echo $message;
